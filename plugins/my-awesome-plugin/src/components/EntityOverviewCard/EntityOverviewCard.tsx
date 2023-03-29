1a579e14b7d753ad429c8de1e2cde6ae039f1291
OmrParlakturk <omer.parlakturk@softtech.com.tr>
2023-03-29 17:33:01 +0300

    my aesome card updated

--- plugins/my-awesome-plugin/src/components/EntityOverviewCard/EntityOverviewCard.tsx
+++ plugins/my-awesome-plugin/src/components/EntityOverviewCard/EntityOverviewCard.tsx
@@ -1,6 +1,30 @@
 import React from 'react';
+import { Typography, Grid } from '@material-ui/core';
+import {
+  InfoCard,
+  Header,
+  Page,
+  Content,
+  ContentHeader,
+  HeaderLabel,
+  SupportButton,
+} from '@backstage/core-components';
+import { ExampleFetchComponent } from '../ExampleFetchComponent';
 
 export const EntityOverviewCard = () => {
 
-    return <div>Hello World</div>;
+    return (
+        <Content>
+          <Grid container spacing={3} direction="column">
+            <Grid item>
+              <InfoCard title="My awesome plugin card">
+                <Typography variant="body1">
+                  Hello from my awesome plugin
+                  {/*use entity hook*/}
+                </Typography>
+              </InfoCard>
+            </Grid>
+          </Grid>
+        </Content>
+    );
 };
